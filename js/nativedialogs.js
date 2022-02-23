331e7bfe8880713c67967c51376726d4ea081218
‘Evan <puzonevan@gmail.com>
2022-02-22 20:13:07 -0800

    native dialog event listeners

---
+++ js/nativedialogs.js
@@ -0,0 +1,19 @@
+// Alert click
+document.getElementById('alert').addEventListener('click', () =>{
+    
+});
+
+// Confirm click
+document.getElementById('confirm').addEventListener('click', () =>{
+    
+});
+
+// Prompt click
+document.getElementById('prompt').addEventListener('click', () =>{
+    
+});
+
+// Safe Prompt click
+document.getElementById('safe-prompt').addEventListener('click', () =>{
+    
+});

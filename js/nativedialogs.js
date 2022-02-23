a00a845aab8a23bfb2d9bd5db03dc3e6378e66e5
‘Evan <puzonevan@gmail.com>
2022-02-22 20:25:29 -0800

    native dialog confirm

--- js/nativedialogs.js
+++ js/nativedialogs.js
@@ -1,11 +1,14 @@
+const output = document.getElementById('output');
+
 // Alert click
 document.getElementById('alert').addEventListener('click', () =>{
-    
+    window.alert('The alert button was clicked');
 });
 
 // Confirm click
 document.getElementById('confirm').addEventListener('click', () =>{
-    
+    let result = window.confirm("Are you sure?");
+    output.innerHTML = `The value returned by the confirm method is: ${result}`;
 });
 
 // Prompt click
